db7f0a33fc6dd05a714d4530a93f83e1941d87e4
kumararnav0 <arnavku4321@gmail.com>
2024-05-02 18:31:00 +0530

    .

---
+++ valuesswitching.py
@@ -0,0 +1,7 @@
+a=int(input("enter first number"))
+b=int(input(" enter second number"))
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
---
+++ variables.py
@@ -0,0 +1,4 @@
+name=input("what is your name")
+print(name)
+name_length=len(name)
+print(name_length)
