940af6a2be7080a5e2c6312b33e92e112ec01cc9
kumararnav0 <arnavku4321@gmail.com>
2024-05-04 19:41:26 +0530

    .

---
+++ logicaloperators.py
@@ -0,0 +1,16 @@
+a=int(input("enter any no"))
+if(a>10 and a<20):
+    print("True")
+else:
+    print("False")
+if(a>20 or a<30):
+    print("true")
+else:
+    print("False")
+if( not a<12):
+    print("True")
+else:
+    print("False")
+
+
+
---
+++ lovecalculator.py
@@ -0,0 +1,30 @@
+print("The Love Calculator is calculating your score...")
+name1 = input("Enter your name \n") # What is your name?
+name2 = input("Enter your crush's name \n") # What is their name?
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+name=name1+name2
+name=name.lower()
+
+t=name.count('t')
+r=name.count('r')
+u=name.count('u')
+e=name.count('e')
+final1=t+r+u+e
+
+l=name.count('l')
+o=name.count('o')
+v=name.count('v')
+e=name.count('e')
+final2=l+o+v+e
+
+res=str(final1)+str(final2)
+print(res)
+
+res=int(res)
+if(res<10 or res>90):
+  print(f"Your score is {res}, you go together like coke and mentos.")
+elif(40<res<50):
+  print(f"Your score is {res}, you are alright together.")
+else:
+  print(f"Your score is {res}.")
