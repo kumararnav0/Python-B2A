7b2a2f4499915750d15f95e9523cf08807d6ab37
kumararnav0 <arnavku4321@gmail.com>
2024-05-08 18:41:12 +0530

    Update bmicalculator.py

--- bmicalculator.py
+++ bmicalculator.py
@@ -1,4 +1,4 @@
 weight=float(input("Enter your weight in kilograms(Kg's) \n"))
 height=float(input("Enter your weight in centimetres(Cm's) \n"))
 bmi=float(weight/((height/100)**2))
-print("Your BMI is equal to ",bmi)
+print("Your BMI is equal to: ",bmi)
