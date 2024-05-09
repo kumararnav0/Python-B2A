fc98baada375da99a05104ec0b9a184b5adbc239
kumararnav0 <arnavku4321@gmail.com>
2024-05-09 21:22:39 +0530

    .

---
+++ func_calculator.py
@@ -0,0 +1,15 @@
+def square(a=int(input("Enter any integer"))):
+    print(f"The square of the number is {a*a} ")
+def add(a=int(input("Enter any integer")) ,b=int(input("Enter any number"))):
+    print(f"The sum of the numbers is {a+b} ")
+def sub(a=int(input("Enter large integer")) ,b=int(input("Enter small number"))):
+    print(f"The sutraction of the numbers is {a-b} ")
+def product(a=int(input("Enter first number")) ,b=int(input("Enter second number"))):
+    print(f"The product of the numbers is {a*b} ")
+def division(a=int(input("Enter first number")) ,b=int(input("Enter second number"))):
+    print(f"The product of the numbers is {a/b} ")
+def remainder(a=int(input("Enter the dividend")) ,b=int(input("Enter the diviser "))):
+    print(f"The remainder of the division is {a%b} ")
+
+
+m=input("What operation would you like to perform \n Addition(A),Subtraction(S),Multiplication(M),Division(D),Square(S),Remainder(R) \n Use code given in brackets as an input")
