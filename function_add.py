fc2ba73715c0c8a72aa8b6b827500c5ed5f7cc03
kumararnav0 <arnavku4321@gmail.com>
2024-05-09 21:06:37 +0530

    .

---
+++ function_add.py
@@ -0,0 +1,5 @@
+
+def add_func(a=int(input("Enter any integer"))):
+    a=a*a
+    print(f"The square of the number is {a} ")
+add_func()
--- greet.py
+++ greet.py
@@ -1,5 +1,6 @@
-def greet():
+def greet(a=5):
     print("Hello")
     print("It's so nice to meet you")
     print("Feels good to see you again")
+    print(a+5)
 greet()
