826acb7baf98f14cdf491b42dffe575570d397ee
kumararnav0 <arnavku4321@gmail.com>
2024-05-11 16:14:38 +0530

    .

---
+++ is_prime.py
@@ -0,0 +1,25 @@
+# Write your code below this line 👇
+def prime_checker(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+
+  
+
+
+
+    
+    
+# Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+if prime_checker(n):
+  print("It's a prime number.")
+else:
+  print("It's not a prime number.")
---
+++ paint_area_calculator.py
@@ -0,0 +1,12 @@
+import math
+
+def paint_calc(height, width, cover):
+  num_cans = (height * width) / cover
+  round_up_cans = math.ceil(num_cans)
+  print(f"You'll need {round_up_cans} cans of paint.")
+
+# Your code above this line 👆
+test_h = int(input()) # Height of wall (m)
+test_w = int(input()) # Width of wall (m)
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
