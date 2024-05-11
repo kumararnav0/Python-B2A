4327a970a35afe0f0d4e02a27348a53313e6675b
kumararnav0 <arnavku4321@gmail.com>
2024-05-11 17:44:22 +0530

    Update dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+dictionary_first={ "1":"Arnav","2":"Anshul","3":"Devis","4":"Gaurav","5":"Sushant"}
+print(dictionary_first)
+
+#adding and element 
+dictionary_first["6"]="Piyush"
+print(dictionary_first)
+
+#updating values
+dictionary_first["6"]="Pratham"
+print(dictionary_first)
+
+
+empty_list=[]
+empty_dictionary={}
