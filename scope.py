d8ad49eff6155cfec36942c0f6eb2a83252c305b
kumararnav0 <arnavku4321@gmail.com>
2024-05-21 20:39:23 +0530

    .

--- scope.py
+++ scope.py
@@ -1,5 +1,7 @@
+a=4
 def drink_portion():
     potion_strength=2 #local variable having local scope
     print(potion_strength)
 drink_portion()
+print(a)
 print(potion_strength)    #will give an error
