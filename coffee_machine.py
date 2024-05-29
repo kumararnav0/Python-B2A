9b64d298550306256386264df57e9813f65c274d
kumararnav0 <arnavku4321@gmail.com>
2024-05-29 23:17:01 +0530

    .

--- coffee_machine.py
+++ coffee_machine.py
@@ -2,8 +2,8 @@ flavours={"A":"Espresso","B":"Latte","C":"Cappucino"}
 recipe={"A":["50ml Water","18g Coffee,0ml Milk"],"B":["200ml Water","24g Coffee","100ml Milk"],"C":["250ml Water","24g Coffee","100ml Milk"]}
 init_coffee={"300ml Water","100g Coffee","200ml Milk"}
 coins={"Penny":"0.01","Dime":"0.10","Quarter":"0.25","Nickel":"0.5"}
-print (init_coffee)
 
-#print report
-#print coin calculation
-#print resourcces left
+
+#todo print report
+#todo print coin calculation
+#todo print resourcces left
