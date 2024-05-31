2ba94d951a5b11002e38fa377170f0390f82f385
kumararnav0 <arnavku4321@gmail.com>
2024-05-31 23:58:05 +0530

    Update nested_lists.py

--- nested_lists.py
+++ nested_lists.py
@@ -2,5 +2,6 @@ fruits = ["Apple", "Orange", "Banana", "Strawberry", "Grape", "Watermelon", "Pin
 vegetables = ["Carrot", "Potato", "Beet", "Radish", "Turnip", "Spinach", "Kale", "Lettuce", "Swiss Chard", "Broccoli", "Cauliflower", "Brussels Sprouts", "Cabbage", "Garlic", "Onion", "Leek", "Shallot", "Pea", "Bean", "Lentil", "Asparagus", "Celery", "Bell Pepper", "Chili Pepper", "Squash", "Zucchini", "Tomato", "Eggplant", "Cucumber"]
 
 
+
 combine=[fruits,vegetables]
 print(combine)
